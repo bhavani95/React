a85a3667a57887a32dd0c00060ec5d8eda390e60
Durga Bhavani Kuppagiri <bhavani.kuppagiri@gmail.com>
2019-01-15 16:17:11 -0500

    this commit contins the usage of gitDidMount,state and gitDidUpdate usage in seasons project

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,40 +17,39 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
+    state = { lat: null, errorMessage: '' };
 
-        super(props);
 
-        this.state = { lat: null, errorMessage : '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
 
-            (position) => {
-                //we called setState!!!
-                this.setState({
-                    lat: position.coords.latitude
-                })
-            },
-            
-            (err) => {
-                this.setState({
-                    errorMessage: err.message
-                })
-            }
+            position => this.setState({lat: position.coords.latitude}) ,
 
-        );
+            err => this.setState({errorMessage: err.message})
+            );
+}
 
 
+    componentDidUpdate() {
+
+        console.log('My component was just Updated and rerendered!');
     }
+
 
     //React Says we have to define render
 
     render() {
 
-        return (<div>Latitude:{this.state.lat} 
-            
-            <br />
-                ErrorMessage: {this.state.errorMessage}</div >);
+        if (this.state.errorMessage && !this.state.render) {
+            return <div> Error :{this.state.errorMessage}</div>
+
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude : {this.state.lat}</div>
+        }
+
+        return <div>Loading!! </div>
 
     }
 }
